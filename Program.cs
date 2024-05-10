ba5c4908687775290cbf57184e071bb75cf35508
Cagimenes <carolmicagimenes@gmail.com>
2024-05-10 09:00:13 -0300

    Alteraçoes

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<Contexto>
-    (options => options.UseSqlServer("Data Source=SB-1490625\\SQLSENAI; " + "Initial Catalog = MOSAIK;Integrated Security = True;TrustServerCertificate = True"));
+    (options => options.UseSqlServer("Data Source=SB-1490624\\SQLSENAI; " + "Initial Catalog = MOSAIK;Integrated Security = True;TrustServerCertificate = True"));
 
 var app = builder.Build();
 
